3330999b58bc9cc1cb96c0cb0b07536c014f6387
Gurnav224 <chaudharyg856@gmail.com>
2025-04-05 19:24:20 +0530

    fixed url name

--- src/config/connect.db.js
+++ src/config/connect.db.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose"
 
 export const connetToDB = async () => {
     try {
-        const connection = await mongoose.connect(process.env.database_url,{dbName:'book_store_DB'});
+        const connection = await mongoose.connect(process.env.MONGO_URI,{dbName:'book_store_DB'});
         if(connection){
             console.log(`successfully connected to database ${connection.connection.db.databaseName}`)
         }
